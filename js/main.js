8e4256e39062d6aa86a439e89fa4d00cb2c95698
Ertan Kara <kara.ertan9@gmail.com>
2018-04-11 23:49:00 +0300

    Add comments explain some long code

--- js/main.js
+++ js/main.js
@@ -1,7 +1,6 @@
 const $addTaskInput = document.querySelector('#add-task');
 const $addTaskButton = document.querySelector('#add-task-button');
 const $taskContainer = document.querySelector('#task-container');
-let $tasks = [...document.querySelectorAll('.task')];
 
 class Task {
   constructor(taskText, isCrossed = false) {
@@ -13,7 +12,6 @@ class Task {
 
 function addTask() {
   "use strict";
-  //const task = $addTaskInput.value;
   const task = new Task($addTaskInput.value);
   // Check if it has valid length
   if (task.taskText.length > 50) {
@@ -21,7 +19,7 @@ function addTask() {
   }
 
   // If it comes this far it means task is valid
-  $tasks = document.querySelectorAll('.task');
+  const $tasks = document.querySelectorAll('.task');
   let numberOfTasks = $tasks.length;
   numberOfTasks++;
 
@@ -43,36 +41,43 @@ function addTask() {
 
 
 $taskContainer.addEventListener('click', event => {
+  // If the to-do item in the container is clicked
   if (event.target.nodeName === 'SPAN') {
+    // Retrieve the task array from the localStorage
     const taskArray = JSON.parse(localStorage.getItem('all-tasks'));
+    // If the clicked element has not crossed over
     if (!event.target.className.match(/done-task/g)) {
+      // Cross over
       event.target.classList.add('done-task');
+      // And also mark it as crossed so on page reload it will remain crossed over
       for (let i = 0; i < taskArray.length; i++) {
-        // event.target.textContent.replace(/[^a-zA-Z ]/g, '').trim
         if (event.target.textContent.replace(/[0-9]- /g, '') ===  taskArray[i].taskText) {
           taskArray[i].isCrossed = true;
           break;
         }
       }
+      // Store task array back after the updates
       localStorage.setItem('all-tasks', JSON.stringify(taskArray));
     } else {
+      // Remove cross over
       event.target.classList.remove('done-task');
+      // Also update the object so it won't be crossed over when page reloads
       for (let i = 0; i < taskArray.length; i++) {
         if (event.target.textContent.replace(/[0-9]- /g, '') === taskArray[i].taskText) {
           taskArray[i].isCrossed = false;
           break;
         }
       }
+      // Store task array back after the updates
       localStorage.setItem('all-tasks', JSON.stringify(taskArray));
     }
   }
 });
 
-
-
-
+// Click event for the button
 $addTaskButton.addEventListener('click', addTask);
 
+// Enter key will also work to add tasks to the list
 document.addEventListener('keydown', event => {
   // If the 'enter' key is pushed while input area is focused
   if (event.keyCode === 13 && document.activeElement === $addTaskInput) {
--- js/on-start.js
+++ js/on-start.js
@@ -1,10 +1,14 @@
 document.addEventListener('DOMContentLoaded', () => {
+  // If localStorage exists in the browser and there are some previous tasks
   if (localStorage && localStorage.getItem('all-tasks')) {
+    // Retrieve the task array
     const taskArray = JSON.parse(localStorage.getItem('all-tasks'));
+    // And create span elements for each task
     for (let i = 0; i < taskArray.length; i++) {
       const task = taskArray[i];
       const newEl = document.createElement('span');
       newEl.classList.add('task');
+      // If task was previously crossed over then cross it over again
       if (task.isCrossed) {
         newEl.classList.add('done-task');
       }
